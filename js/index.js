7fe36a097a82bcd9a2d947114835caf24feb8f39
Andrew Bryner <bryner.and@gmail.com>
2015-10-27 01:25:21 -0400

    selector changes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 $("#regions").change(function () {
 	event.preventDefault();
 	var $selectedOption = $("#regions :selected").val();
-    alert($selectedOption);
     window.location.href=$(this).val()
 
 });
