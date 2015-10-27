1b853d19881bf6e4cbb6ce3de1917083d9c7073a
Andrew Bryner <bryner.and@gmail.com>
2015-10-26 21:20:53 -0400

    test

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// $('#marker-ig8kb8pk1').on('click', function() {
+// 	$('html body .main-content').animate({
+//             scrollTop: $("#test").offset().top
+//                     }, 2000);
+// });
+
+$('#1ff062da82bdf2e12aa4e91d61941a0f').on('click', function() {
+	alert('hi');
+})
