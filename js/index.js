3885b2fd6dbc4006247579cd5500ebb0d358057c
Andrew Bryner <bryner.and@gmail.com>
2015-10-27 01:17:26 -0400

    changes late

--- js/index.js
+++ js/index.js
@@ -1,9 +1,7 @@
-// $('#marker-ig8kb8pk1').on('click', function() {
-// 	$('html body .main-content').animate({
-//             scrollTop: $("#test").offset().top
-//                     }, 2000);
-// });
+$("#regions").change(function () {
+	event.preventDefault();
+	var $selectedOption = $("#regions :selected").val();
+    alert($selectedOption);
+    window.location.href=$(this).val()
 
-$('#1ff062da82bdf2e12aa4e91d61941a0f').on('click', function() {
-	alert('hi');
-})
+});
