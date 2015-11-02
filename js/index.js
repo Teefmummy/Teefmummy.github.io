2009cb974ba1054a6c44b9f04fb94720b3ddc9ca
Andrew Bryner <bryner.and@gmail.com>
2015-11-01 22:28:29 -0500

    bigguns

--- js/index.js
+++ js/index.js
@@ -3,3 +3,31 @@ $("#regions").change(function () {
 	var $selectedOption = $("#regions :selected").val();
     window.location.href=$(this).val();
 });
+
+$('.read-more-food').on('click', function(event) {
+	event.preventDefault();
+	$('.hidden-text-food').slideDown();
+	$('.read-more-food').hide();
+	$('.read-less-food').show();
+});
+
+$('.read-less-food').on('click', function(event) {
+	event.preventDefault();
+	$('.hidden-text-food').slideUp();
+	$('.read-more-food').show();
+	$('.read-less-food').hide();
+});
+
+$('.read-more').on('click', function(event) {
+	event.preventDefault();
+	$('.hidden-text').slideDown();
+	$('.read-more').hide();
+	$('.read-less').show();
+});
+
+$('.read-less').on('click', function(event) {
+	event.preventDefault();
+	$('.hidden-text').slideUp();
+	$('.read-more').show();
+	$('.read-less').hide();
+});
